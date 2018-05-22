012823c2001d3d5124bd5f4bed6934c8739b182b
Libor Zahradka <liborzahradka@hotmail.com>
2018-05-22 16:01:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# GREET | API DOCUMENTATION (DEMO/TEMPLATE)
+
+Api documentation made by GREET team, fully responsive, easy to edit.
+
+## Getting Started
+
+You can find full product on [greet.chat](https://www.greet.chat/api_documentation)
+
+## Built With
+
+* HTML
+* CSS
+* JS (jQuery)
+
+## Contributing
+
+Feel free to contribute on this project.
+
+## Versioning
+
+We use [Heroku](https://heroku.com/) for versioning and deploy.
+
+## Authors
+
+* **Libor Zahradka** - *Initial work* - [GitHub](https://github.com/liborzahradka)
+* **Cameron Burley** - *Initial work* - [GitHub](https://github.com/)
+
+## Acknowledgments
+
+* [HIGHLIGHT.JS](https://highlightjs.org)
---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  // LOAD SYNTAX HIGHLIGHT
+  hljs.initHighlighting();
+
+  // REMOVE EXTRA TABS FOR CODE
+  $("pre code").each(function(){
+    var html = $(this).html();
+    var pattern = html.match(/\s*\n[\t\s]*/);
+    $(this).html(html.replace(new RegExp(pattern, "g"),'\n'));
+  });
+
+  // CODE MENU & CODE SWITCH
+  var method_menu = $('.method-menu li');
+  $('.request code').hide();
+  $('.request code.bash').show();
+  method_menu.click(function(){
+    var lang = $(this).data('lang');
+
+    method_menu.removeClass('active');
+    $(this).addClass('active');
+
+    $('.request code').hide();
+    $('.request code.'+ lang +'').show();
+
+  });
+
+  $('.mobile-menu').click(function(){
+    $('.navigation').toggleClass('mobile');
+  });
+
+  // MAIN MENU HIGHLIGHT WHILE SCROLL
+  var last_id,
+   top_menu = $(".navigation .menu"),
+   top_menu_height = top_menu.outerHeight()+1,
+   menu_items = top_menu.find("a"),
+   scroll_items = menu_items.map(function(){
+     var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+   });
+
+  menu_items.click(function(e){
+    var href = $(this).attr("href"),
+        offset_top = href === "#" ? 0 : $(href).offset().top-top_menu_height+100;
+    $('html, body').stop().animate({
+        scrollTop: offset_top
+    }, 850);
+    e.preventDefault();
+  });
+
+  $(window).scroll(function(){
+     var from_top = $(this).scrollTop()+top_menu_height;
+     var cur = scroll_items.map(function(){
+       if ($(this).offset().top < from_top)
+         return this;
+     });
+
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+
+     if (last_id !== id) {
+         last_id = id;
+         menu_items
+           .parent().removeClass("active")
+           .end().filter("[href=\\#"+id+"]").parent().addClass("active");
+     }
+  });
+});
